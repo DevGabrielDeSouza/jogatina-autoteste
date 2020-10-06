0d3eb93e67fa19bac7a76edad2e532d8cba7a5f4
GabrielDeSouza <devgabrieldesouza@gmail.com>
2020-10-06 03:05:16 -0300

    Open side menu
    Open side menu with the option to change the avatar image.

--- jogatest.js
+++ jogatest.js
@@ -4,17 +4,16 @@ const timeoutWait = 10000;
 
 let driver;
 
-
 async function TextInElement(path, text) {
 	await driver.findElement(By.xpath(path)).sendKeys(text);
 }
 
-async function ClickInElement(path) {
+async function ClickElement(path) {
 
 	await driver.findElement(By.xpath(path)).click();
 }
 
-async function ClickInVisibleElement(path) {
+async function ClickElementWhenVisible(path) {
 	errorInLastTest = false;
 	try {
 		await driver.wait(until.elementIsVisible(driver.findElement(By.xpath(path))), timeoutWait).then(async el => {
@@ -26,19 +25,14 @@ async function ClickInVisibleElement(path) {
 					+ e + "\n\nCom o path: \n" + path +
 					"\n==================================================\n\n")
 			}
-			/*el.click().catch(
-				e => console.log("\n\n==================================================\nErro no click: \n"
-				+ e + "\n\nCom o path: \n" + path +
-				"\n==================================================\n\n")
-			);*/
 
-		})/*.catch(e => console.log("Erro1: " + e));*/
+		})
 	} catch (e) {
 
 		console.log("\nErro ao buscar elemento: " + e + "\nCom o path: " + path + "\n");
 		errorInLastTest = true;
 
-		/*console.log("Nome: " + (e).constructor.toString());*/
+		/*DebugClass(e);*/
 
 		/*if (e instanceof NoSuchElementError) {
 			console.log("Elemento não encontrado!");
@@ -50,17 +44,34 @@ async function ClickInVisibleElement(path) {
 
 async function LoginFirstPage(username, password) {
 
-	await ClickInElement("/html/body/div[1]/div/div[2]/a");
-	//await ClickInVisibleElement("/html/body/div[1]/div/div[37]/a");
+	await ClickElement("/html/body/div[1]/div/div[2]/a");
+	//await ClickElementWhenVisible("/html/body/div[1]/div/div[37]/a");
 
 	await TextInElement("/html/body/div[1]/div/div[2]/div/form/div[1]/input", username);
 	await TextInElement("/html/body/div[1]/div/div[2]/div/form/div[3]/input", password);
 
 	//confirm
-	await ClickInElement("/html/body/div[1]/div/div[2]/div/form/input");
-
-	//await ClickInVisibleElement("/html/body/div[1]/div/div[2]/div/form/input");
+	await ClickElement("/html/body/div[1]/div/div[2]/div/form/input");
 }
+
+async function ChangeAvatar(avatarPath) {
+	await driver.switchTo().defaultContent();
+
+
+	//Move pointer to user side menu
+	const actions = driver.actions({ bridge: true });
+
+	await driver.findElement(By.xpath("/html/body/div[1]/div/ul/li[6]/a/i")).then(elem => {
+		actions.move({ origin: elem })
+			.press();
+	});
+	await actions.perform();
+
+	//Open change avatar image option
+	await ClickElement("/html/body/div[1]/div/ul/li[6]/ul/li[9]");
+
+}
+
 
 
 (async function example() {
@@ -73,8 +84,16 @@ async function LoginFirstPage(username, password) {
 
 		await LoginFirstPage("gab@test.com", "teste123");
 
+
+		await ChangeAvatar("/html/body/form/div[1]/div/ul/li[18]/label/img");
+
 	}
 	finally {
 		driver.quit();
 	}
 })();
+
+
+function DebugClass(e){
+	console.log("Nome: " + (e).constructor.toString());
+}
