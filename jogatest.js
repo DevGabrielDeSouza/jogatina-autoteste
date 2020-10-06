d76414b71a04188fb8c6dcc5adf24fe7655f2e11
GabrielDeSouza <devgabrieldesouza@gmail.com>
2020-10-06 04:25:24 -0300

    Changing user Avatar
    Script finally capable of changing the avatar of the current user multiple times in a row.

--- jogatest.js
+++ jogatest.js
@@ -70,12 +70,36 @@ async function ChangeAvatar(avatarPath) {
 	//Open change avatar image option
 	await ClickElement("/html/body/div[1]/div/ul/li[6]/ul/li[9]");
 
+	await SelectAvatar(avatarPath);
+
+}
+
+async function SelectAvatar(avatarPath) {
+
+	await driver.switchTo().defaultContent();
+	await driver.switchTo().frame(driver.findElement(By.xpath("/html/body/div[7]/div[1]/div[2]/div[2]/div[1]/iframe")));
+
+	let startMessage = await driver.findElement(By.xpath("/html/body/div[2]/div/div")).getText();
+
+	//click in the new avatar
+	await ClickElement(avatarPath);
+
+	//click in confirm avatar change
+	await ClickElement("/html/body/form/div[2]/div/div[2]/input");
+
+	//debug result of the avatar change
+	let currentMessage = await driver.findElement(By.xpath("/html/body/div[2]/div/div")).getText();
+
+	
+	//click in the close frame button
+	await driver.switchTo().defaultContent();
+	await ClickElement("/html/body/div[7]/div[1]/div[2]/div[2]/button[4]");
 }
 
 
 
 (async function example() {
-	driver = await new Builder().forBrowser('firefox').build();
+	driver = await new Builder().forBrowser('chrome').build();
 	try {
 		// Navigate to Url
 		await driver.get("http://www.jogatina.com/");
@@ -85,7 +109,17 @@ async function ChangeAvatar(avatarPath) {
 		await LoginFirstPage("gab@test.com", "teste123");
 
 
-		await ChangeAvatar("/html/body/form/div[1]/div/ul/li[18]/label/img");
+		try{await ChangeAvatar("/html/body/form/div[1]/div/ul/li[18]/label/img");}catch{
+			await driver.get("https://www.jogatina.com/welcome.do");
+		}
+		await ChangeAvatar("/html/body/form/div[1]/div/ul/li[17]/label/img");
+		await ChangeAvatar("/html/body/form/div[1]/div/ul/li[16]/label/img");
+		await ChangeAvatar("/html/body/form/div[1]/div/ul/li[15]/label/img");
+		await ChangeAvatar("/html/body/form/div[1]/div/ul/li[14]/label/img");
+		await ChangeAvatar("/html/body/form/div[1]/div/ul/li[13]/label/img");
+		await ChangeAvatar("/html/body/form/div[1]/div/ul/li[12]/label/img");
+		await ChangeAvatar("/html/body/form/div[1]/div/ul/li[11]/label/img");
+		await ChangeAvatar("/html/body/form/div[1]/div/ul/li[10]/label/img");
 
 	}
 	finally {
