83026b38b82e402e2d3f9e0812fb78331b4d2958
GabrielDeSouza <devgabrieldesouza@gmail.com>
2020-10-09 06:39:07 -0300

    Profile/Avatar Popup and SideMenu
    - Added sidemenupage
    - Added routines to open the avatar and profile change popups

--- pageobjects/loginpage.js
+++ pageobjects/loginpage.js
@@ -2,7 +2,7 @@ function DriverUtils(_driver) {
 	this.driver = _driver;
 }
 
-const ElmentsPaths = {
+const ElementsPaths = {
 	OpenLoginButton: "/html/body/div[1]/div/div[2]/a",
 	InputEmailField: "/html/body/div[1]/div/div[2]/div/form/div[1]/input",
 	InputPasswordField: "html/body/div[1]/div/div[2]/div/form/div[3]/input",
@@ -11,29 +11,28 @@ const ElmentsPaths = {
 
 module.exports ={
 	driver: DriverUtils,
-	getDriver : function () {
-		return this.driver;
-	},
 	initialize : function (_driver) {
 		this.driver = _driver;
 	},
 	openLogin : async function() {
-		await this.driver.clickElement(ElmentsPaths.OpenLoginButton, this.driver.PathType.XPATH);
+		return await this.driver.clickElement(ElementsPaths.OpenLoginButton, this.driver.PathType.XPATH);
 	},
 	inputEmail: async function(email) {
-		await this.driver.inputTextElement(ElmentsPaths.InputEmailField, email, this.driver.PathType.XPATH);
+		return await this.driver.inputTextElement(ElementsPaths.InputEmailField, email, this.driver.PathType.XPATH);
 	},
 	inputPassword : async function(password) {
-		await this.driver.inputTextElement(ElmentsPaths.InputPasswordField, password, this.driver.PathType.XPATH);
+		return await this.driver.inputTextElement(ElementsPaths.InputPasswordField, password, this.driver.PathType.XPATH);
 	},
 	confirmLogin: async function () {
-		await this.driver.clickElement(ElmentsPaths.ConfirmLoginButton, this.driver.PathType.XPATH);
+		return await this.driver.clickElement(ElementsPaths.ConfirmLoginButton, this.driver.PathType.XPATH);
 	},
 	login : async function (email, password) {
 
-		await this.openLogin();
-		await this.inputEmail(email);
-		await this.inputPassword(password);
-		await this.confirmLogin();
+		return await Promise.all([
+			(await this.openLogin()), 
+			(await this.inputEmail(email)), 
+			(await this.inputPassword(password)), 
+			(await this.confirmLogin())
+		]);
 	}
 }
