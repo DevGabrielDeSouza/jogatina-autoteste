be5f0e4515edccc8ea7df3dbbcc0a2239d3e7dfa
GabrielDeSouza <devgabrieldesouza@gmail.com>
2020-10-08 23:37:11 -0300

    WebDriverUtils basic methods and errors
    - driver initialization, quit, go to page, implicit/explicit wait methods;
    - find element, input/text fields methods;

--- drivertest.js
+++ drivertest.js
@@ -1,11 +1,41 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
+
 const Driver = require('./webdriverutils');
 
 (async function example() {
-	Driver.initializeFirefox();
+	Driver.initializeChrome();
 
 	try {
 		Driver.goToPage("http://www.jogatina.com/");
-		await Driver.sleep(10000);
+		Driver.setImplicitTimeOut(10000);
+
+
+		//await Driver.sleep(6000);
+
+		//await Driver.driver.findElement(By.xpath("/html/body/div[2]/div/div/div[1]/form/div/fieldset[3]/input")).click();
+
+
+
+		//Path routines to test errors:
+		//await Driver.findElement("/html/body/div[2]/div/div/div[1]/form/div/fieldset[3]/input", Driver.PathType.XPATH);
+		//await Driver.clickElement("/html/body/div[2]/div/div/div[1]/form/div/fieldset[3]/input", Driver.PathType.XPATH);
+		//await Driver.findElement("/html/body/div[2]/div/div/div[1]/form/div/fieldset[355]/input", Driver.PathType.XPATH);
+		//await Driver.clickElement("/html/body/div[2]/div/div/div[1]/form/div/fieldset[355]/input", Driver.PathType.XPATH);
+
+		//Id routines to test errors:
+		//await Driver.findElement("botao", Driver.PathType.ID);
+		//await Driver.clickElement("botao", Driver.PathType.ID);
+		//await Driver.findElement("zzzqqq", Driver.PathType.ID);
+		//await Driver.clickElement("zzzqqq", Driver.PathType.ID);
+
+
+
+
+
+		//await Driver.clickElement("/html/body/div[1]/div/div[2]/a", Driver.PathType.XPATH);
+
+
+		await Driver.sleep(6000);
 
 	}
 	finally {
--- webdriverutils.js
+++ webdriverutils.js
@@ -4,8 +4,26 @@ function Driver(_driver) {
 	this.driver = _driver;
 }
 
+function errorBehaviour(error) {
+	throw error;
+}
+
+function debugError(e, action, path){
+	console.log("\n\n\n==================================================\nError in module:\n"
+		+"webdriverutils.js\n\nTrigger action:\n"
+		+ action +"\n\nError:\n"
+		+ e + "\n\nWebElement with path:\n" + path
+		+"\n==================================================\n\n\n"
+	);
+}
+
 module.exports = {
 	driver: Driver,
+
+	PathType : Object.freeze({
+		XPATH: 0,
+		ID: 1
+	}),
 
 	initializeFirefox: async function () {
 		this.driver = new Builder().forBrowser("firefox").build();
@@ -25,57 +43,51 @@ module.exports = {
 	sleep: async function (time) {
 		await this.driver.sleep(time);
 	},
-	teste: function () {
-		console.log("teste OK!!");
+	findElement: async function (path, type) {
+		switch (type) {
+			case this.PathType.XPATH:
+				try {
+					return await this.driver.findElement(By.xpath(path));
+				} catch (e) {
+					debugError(e, "Trying to find WebElement.", path),
+					errorBehaviour(e);
+				}
+				break;
+			case this.PathType.ID:
+				try {
+					return await this.driver.findElement(By.id(path));
+				} catch (e) {
+					debugError(e, "Trying to find WebElement.", path),
+					errorBehaviour(e);
+				}
+				break;
+			default:
+				console.log("Invalid path type.");
+				break;
+		}
 	},
 	clickElement: async function (path, type) {
-		switch (type) {
-			case PathType.XPATH:
-				try {
-					await driver.findElement(By.xpath(path)).click();
-				} catch (e) {
-					console.log("\n\n==================================================\nErro no click: \n"
-						+ e + "\n\nCom o path: \n" + path +
-						"\n==================================================\n\n");
-				}
-				break;
-			case PathType.ID:
-				try {
-					await driver.findElement(By.id(path)).click();
-				} catch (e) {
-					console.log("\n\n==================================================\nErro no click: \n"
-						+ e + "\n\nCom o path: \n" + path +
-						"\n==================================================\n\n");
-				}
-				break;
-			default:
-				console.log("Invalid path type.");
-				break;
-		}
+		(await this.findElement(path, type)).click().catch(
+			e => {
+				debugError(e, "Trying to click element.", path),
+				errorBehaviour(e)
+			}
+		);
 	},
 	inputTextElement: async function (path, text, type) {
-		switch (type) {
-			case PathType.XPATH:
-				try {
-					await driver.findElement(By.xpath(path)).sendKeys(text);
-				} catch (e) {
-					console.log("\n\n==================================================\nErro no click: \n"
-						+ e + "\n\nCom o path: \n" + path +
-						"\n==================================================\n\n");
-				}
-				break;
-			case PathType.ID:
-				try {
-					await driver.findElement(By.id(path)).sendKeys(text);
-				} catch (e) {
-					console.log("\n\n==================================================\nErro no click: \n"
-						+ e + "\n\nCom o path: \n" + path +
-						"\n==================================================\n\n");
-				}
-				break;
-			default:
-				console.log("Invalid path type.");
-				break;
-		}
+		(await this.findElement(path, type)).sendKeys(text).catch(
+			e => {
+				debugError(e, "Trying to input text.", path),
+				errorBehaviour(e)
+			}
+		);
+	},
+	clearTextElement : async function (path) {
+		(await this.findElement(path, type)).clear().catch(
+			e => {
+				debugError(e, "Trying to clear text in WebElement.", path),
+				errorBehaviour(e)
+			}
+		);
 	}
 }
