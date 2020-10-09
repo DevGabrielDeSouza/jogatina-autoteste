be84daa54fa225fb0acea7c1743bae8102e9f292
GabrielDeSouza <devgabrieldesouza@gmail.com>
2020-10-09 07:37:25 -0300

    Change Avatar
    - Added changeavatarpage
    - Added change avatar routine

--- pageobjects/changeavatarpage.js
+++ pageobjects/changeavatarpage.js
@@ -2,38 +2,50 @@ function DriverUtils(_driver) {
 	this.driver = _driver;
 }
 
+///html/body/form/div[1]/div/ul/li[6]/label/img
+///html/body/form/div[1]/div/ul/li[6]/label/img
 const ElementsPaths = {
-	OpenLoginButton: "/html/body/div[1]/div/div[2]/a",
-	InputEmailField: "/html/body/div[1]/div/div[2]/div/form/div[1]/input",
-	InputPasswordField: "html/body/div[1]/div/div[2]/div/form/div[3]/input",
-	ConfirmLoginButton: "/html/body/div[1]/div/div[2]/div/form/input"
+	PopupFrame : "/html/body/div[7]/div[1]/div[2]/div[2]/div[1]/iframe",
+	Avatares : function(id){
+		return "/html/body/form/div[1]/div/ul/li[" + id + "]/label/img"
+	},
+	ConfirmButton:  "/html/body/form/div[2]/div/div[2]/input",
+	FeedbackText: "/html/body/div[2]/div/div/span"
 }
 
+let sleepWait = 3000;
+
 module.exports = {
-	driver: DriverUtils,
-	getDriver: function () {
-		return this.driver;
-	},
-	initialize: function (_driver) {
+	driver : DriverUtils,
+	initialize : function (_driver) {
 		this.driver = _driver;
 	},
-	openLogin: async function () {
-		await this.driver.clickElement(ElementsPaths.OpenLoginButton, this.driver.PathType.XPATH);
+	moveToFrame : async function () {
+		return await this.driver.moveToFrame(ElementsPaths.PopupFrame, this.driver.PathType.XPATH);
 	},
-	inputEmail: async function (email) {
-		await this.driver.inputTextElement(ElementsPaths.InputEmailField, email, this.driver.PathType.XPATH);
+	optionAvatar : async function (id) {
+		return await this.driver.clickElement(ElementsPaths.Avatares(id), this.driver.PathType.XPATH);
+		//return await this.driver.clickElement("/html/body/form/div[1]/div/ul/li[17]/label/img", this.driver.PathType.XPATH);
 	},
-	inputPassword: async function (password) {
-		await this.driver.inputTextElement(ElementsPaths.InputPasswordField, password, this.driver.PathType.XPATH);
+	confirmButton : async function () {
+		return await this.driver.clickElement(ElementsPaths.ConfirmButton, this.driver.PathType.XPATH);
 	},
-	confirmLogin: async function () {
-		await this.driver.clickElement(ElementsPaths.ConfirmLoginButton, this.driver.PathType.XPATH);
-	},
-	login: async function (email, password) {
+	getFeedback : async function () {
 
-		await this.openLogin();
-		await this.inputEmail(email);
-		await this.inputPassword(password);
-		await this.confirmLogin();
+		await this.driver.sleep(sleepWait);
+
+		let text = await this.driver.getTextElement(ElementsPaths.FeedbackText, this.driver.PathType.XPATH);
+
+		console.log(text);
+
+		return text;
+	},
+	selectAvatarRoutine : async function(id){
+		return await Promise.all([
+			(await this.moveToFrame()),
+			(await this.optionAvatar(id)),
+			(await this.confirmButton()),
+			(await this.getFeedback())
+		]);
 	}
 }
